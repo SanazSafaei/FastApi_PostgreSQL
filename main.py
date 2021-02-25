88aea393402d9612b35bfae39f2492952ac1c8a3
sanaz <sanazsafaei97@gmail.com>
2021-02-25 17:54:09 +0330

    Improves nameings and functions

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 from aiofile import AIOFile,async_open,LineReader, Writer
 import ast
 import aiohttp
-import asyncio, urllib
+import asyncio
 
 
 class User_base(BaseModel):
@@ -17,42 +17,37 @@ class User(User_base):
 app = FastAPI()
 
 @app.post("/signup/")
-async def sign_up(user: User= Body(...)):
-    user_str = str(user.dict())+"\n"
-    user_all=[]
-    async with AIOFile("Document.txt", 'a+') as doc:
+async def sign_up(user_query: User= Body(...)):
+    user_query_str = str(user_query.dict())+"\n"
+    async with AIOFile("Credentials.txt", 'a+') as doc:
         writer = Writer(doc)
         async for line in LineReader(doc):
-            data = line.replace('\n','')
-            old_user = ast.literal_eval(data)
-            if user.UserName == old_user["UserName"]:
+            line = line.replace('\n','')
+            user_dict = ast.literal_eval(line)
+            if user_query.UserName == user_dict["UserName"]:
                 return {"result" : "User exists!"}
-        await writer(user_str)
-        doc.close()
+        await writer(user_query_str)
         return {"result" : "Congrats you singed up!"}
 
 
 @app.post("/signin/")
-async def sign_in(*, user: str = Form(...), password: str=Form(...), city: str):
-    session = False
-    async with AIOFile("Document.txt", 'r') as doc:
+async def sign_in(*, username: str = Form(...), password: str=Form(...)):
+    async with AIOFile("Credentials.txt", 'r') as doc:
         async for line in LineReader(doc):
-            data = line.replace('\n','')
-            old_user = ast.literal_eval(data)
-            if user == old_user["UserName"]:
-                if password == old_user ["Password"]:
-                    session= True
-                else:
-                     return {"result" : "Password is wrong."}
-        doc.close()
-    if session :
-        address = "https://api.openweathermap.org/data/2.5/weather?q="+ city+ "&appid=1e2f56bd489cf75c7ad85ab8b2b6eaf4"
-        async with aiohttp.ClientSession(trust_env=True) as session:
-            params = {'q': city, 'appid': '1e2f56bd489cf75c7ad85ab8b2b6eaf4'}
-            async with session.get(address) as  resp:
-                result = await resp.json()
-                return {"result": result}
-    else:
-        return {"result" : "Please sing up first."}
+            line = line.replace('\n','')
+            user_dict = ast.literal_eval(line)
+            print(username, password , user_dict["UserName"], user_dict ["Password"])
+            if username == user_dict["UserName"] and password==user_dict ["Password"]:
+                return {"result" : "You are loged in"}
+        else:
+            return {"result" : "Username or Password is wrong."}   
 
 
+@app.get("/weather/{city}")
+async def weather_provider(city: str):
+    address = "https://api.openweathermap.org/data/2.5/weather"
+    async with aiohttp.ClientSession(trust_env=True) as weatherApi_session:
+        params = {'q': city, 'appid': '1e2f56bd489cf75c7ad85ab8b2b6eaf4'}
+        async with weatherApi_session.get(address, params=params) as  resp:
+            result = await resp.json()
+            return {"result": result}
