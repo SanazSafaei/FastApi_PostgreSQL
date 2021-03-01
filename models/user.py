316c7d7107635c46ee07f544f98a9ca6c75e46ce
sanaz <sanazsafaei97@gmail.com>
2021-03-01 15:57:01 +0330

    switch email with password in User and User_base models

--- models/user.py
+++ models/user.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 
 class User_base(BaseModel):
     username: str = None
-    password: str = None
+    email: str = None
 
 class User(User_base):
-    email: str = None
+    password: str = None
