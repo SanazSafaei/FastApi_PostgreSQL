3cf20f1ad6e120c1fcdc95c8f48ca9b4e8a9747d
sanaz <sanazsafaei97@gmail.com>
2021-03-01 13:51:36 +0330

    modify config_file and add default

--- configuration/config_file.py
+++ configuration/config_file.py
@@ -7,12 +7,15 @@ config = Config("configuration/.env")
 SECRET_KEY: Secret = config("SECRET_KEY", cast=str)
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
-ALLOWED_HOSTS : List[str] = config("ALLOWED_HOSTS", cast=CommaSeparatedStrings)
+ALLOWED_HOSTS : List[str] = config("ALLOWED_HOSTS", cast=CommaSeparatedStrings, default= "*") 
 
-#############################################
+#mongodb
+
 DATABASE_NAME= config("DATABASE_NAME")
-MONGODB_HOST = config("MONGODB_HOST")
-MONGODB_PORT = config("MONGODB_PORT")
-REDIS_HOST = config("REDIS_HOST")
-REDIS_PORT = config("REDIS_PORT")
-REDIS_PASSWORD = config("REDIS_PASSWORD")
+MONGODB_HOST = config("MONGODB_HOST", default= "mongodb://127.0.0.1:27017" )
+MONGODB_PORT = config("MONGODB_PORT", default= "27017")
+
+#redis
+REDIS_HOST = config("REDIS_HOST", default= "localhost")
+REDIS_PORT = config("REDIS_PORT", default= "6379")
+REDIS_PASSWORD = config("REDIS_PASSWORD", default= "")
