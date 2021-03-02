77ad43a6a9d2c56215a6df8fcb814581dd11219e
sanaz <sanazsafaei97@gmail.com>
2021-03-02 15:13:05 +0330

    add is_active to user_base model

--- models/user.py
+++ models/user.py
@@ -4,6 +4,8 @@ from pydantic import BaseModel
 class User_base(BaseModel):
     username: str = None
     email: str = None
+    is_active: bool = False
 
 class User(User_base):
     password: str = None
+    
