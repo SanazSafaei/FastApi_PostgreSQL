0db46c40b534a27a665aea104c29a4236b10e3d5
sanaz <sanazsafaei97@gmail.com>
2021-03-09 19:18:39 +0330

    add get_admin_permission

--- account_manager/api_admin.py
+++ account_manager/api_admin.py
@@ -9,7 +9,7 @@ from db.postgresql_db import get_db
 from models.user import User_create, User_base
 from authentication.security import verify_password, get_password_hash
 from authentication.utils import update_user_state
-from account_manager.utils import get_user_db, email_is_available, update_user
+from account_manager.utils import get_user_db, email_is_available, update_user, get_admin_permission
 
 modify_accounts_router = APIRouter(prefix="/modify_accounts", tags=["admin pannel"])
 
@@ -17,22 +17,22 @@ modify_accounts_router = APIRouter(prefix="/modify_accounts", tags=["admin panne
 async def modify_password_accounts(
     account_username : str = Form(...),
     account_new_password : str = Form(...),
-    user: User_base = Depends(get_current_user),
+    admin: User_base = Depends(get_admin_permission),
     db: Session = Depends(get_db),
 ):
-    user_db = get_user_db(username = user.username , conn = db)
-    if user_db.role != 1:
-        raise HTTPException(
-            status_code=HTTP_400_BAD_REQUEST,
-            detail="Sorry, you don't have access!",
-        )
-    else:
-        password_hash = get_password_hash(account_new_password)
-        update_user(
-            username= account_username,
-            conn=db,
-            field_to_update="password",
-            new_var=password_hash,
-        )
-        return {"Response": "Password successfully updated."}
-    
+    password_hash = get_password_hash(account_new_password)
+    update_user(
+        username= account_username,
+        conn=db,
+        field_to_update="password",
+        new_var=password_hash,
+    )
+    return {"Response": "Password successfully updated."}
+
+# @modify_accounts_router.post("/change-role")
+# async def modify_password_accounts(
+#     account_username : str = Form(...),
+#     role : str = Form(...),
+#     user: User_base = Depends(get_current_user),
+#     db: Session = Depends(get_db),
+# ):
--- account_manager/utils.py
+++ account_manager/utils.py
@@ -1,8 +1,22 @@
 from sqlalchemy.orm import Session
+from fastapi import Depends, HTTPException
+from starlette.status import HTTP_401_UNAUTHORIZED
 from loguru import logger
 
+from authentication.security import verify_token, decode_token
 from db import models
 
+async def get_admin_permission(token=Depends(verify_token)):
+    credentials_exception = HTTPException(
+        status_code=HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials, sorry you don't have access",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    user = decode_token(token)
+    if (user is None) or not (user.is_active) or user.role != 1:
+        logger.error(user)
+        raise credentials_exception
+    return user
 
 def get_user_db(username: str, conn: Session):
     user_db = conn.query(models.User).filter(models.User.username == username).first()
