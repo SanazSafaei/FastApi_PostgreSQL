a36568388306ae016f44598a6f13ab7cb4b9e7e9
sanaz <sanazsafaei97@gmail.com>
2021-03-09 19:49:10 +0330

    add cahnge_role, change_email, confirm_email to admin's panel

--- account_manager/api_admin.py
+++ account_manager/api_admin.py
@@ -9,30 +9,90 @@ from db.postgresql_db import get_db
 from models.user import User_create, User_base
 from authentication.security import verify_password, get_password_hash
 from authentication.utils import update_user_state
-from account_manager.utils import get_user_db, email_is_available, update_user, get_admin_permission
+from account_manager.utils import (
+    get_user_db,
+    email_is_available,
+    update_user,
+    get_admin_permission,
+)
 
 modify_accounts_router = APIRouter(prefix="/modify_accounts", tags=["admin pannel"])
 
+
 @modify_accounts_router.post("/password")
 async def modify_password_accounts(
-    account_username : str = Form(...),
-    account_new_password : str = Form(...),
+    account_username: str = Form(...),
+    account_new_password: str = Form(...),
     admin: User_base = Depends(get_admin_permission),
     db: Session = Depends(get_db),
 ):
     password_hash = get_password_hash(account_new_password)
     update_user(
-        username= account_username,
+        username=account_username,
         conn=db,
         field_to_update="password",
         new_var=password_hash,
     )
-    return {"Response": "Password successfully updated."}
+    return {"Response": "User's password successfully updated."}
 
-# @modify_accounts_router.post("/change-role")
-# async def modify_password_accounts(
-#     account_username : str = Form(...),
-#     role : str = Form(...),
-#     user: User_base = Depends(get_current_user),
-#     db: Session = Depends(get_db),
-# ):
+
+@modify_accounts_router.post("/change-role")
+async def modify_role_accounts(
+    account_username: str = Form(...),
+    account_new_role: str = Form(...),
+    user: User_base = Depends(get_admin_permission),
+    db: Session = Depends(get_db),
+):
+    if account_new_role == "admin":
+        role = 1
+    elif account_new_role == "user":
+        role = 2
+    else:
+        raise HTTPException(
+            status_code=HTTP_400_BAD_REQUEST,
+            detail="This role doesn't exsits!",
+        )
+    update_user(
+        username=account_username,
+        conn=db,
+        field_to_update="role",
+        new_var=role,
+    )
+    return {"Response": "User's role successfully updated."}
+
+
+@modify_accounts_router.post("/change-email")
+async def modify_email_accounts(
+    account_username: str = Form(...),
+    account_new_email: str = Form(...),
+    user: User_base = Depends(get_admin_permission),
+    db: Session = Depends(get_db),
+):
+    if not email_is_available(username=account_username, conn=db, email=account_new_email):
+        raise HTTPException(
+            status_code=HTTP_400_BAD_REQUEST,
+            detail="There is an account with the new email!",
+        )
+    update_user(
+        username=account_username,
+        conn=db,
+        field_to_update="email",
+        new_var=account_new_email,
+    )
+    update_user_state(username=account_username, state=False, conn=db)
+    return {"Response": "User's email successfully updated. Please confrim the email."}
+
+
+@modify_accounts_router.post("/confirm-email")
+async def modify_confirm_email_accounts(
+    account_username: str = Form(...),
+    user: User_base = Depends(get_admin_permission),
+    db: Session = Depends(get_db),
+):
+    update_user(
+        username=account_username,
+        conn=db,
+        field_to_update="is_active",
+        new_var=True,
+    )
+    return {"Response": "User's email successfully confirmed."}
