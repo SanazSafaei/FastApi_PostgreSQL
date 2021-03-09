1d738c1d7a6e03530424808ff730ab834faa2c2f
sanaz <sanazsafaei97@gmail.com>
2021-03-09 17:29:30 +0330

    fix changing user's state bug and add email checking to check new email is avaliable

--- account_manager/api.py
+++ account_manager/api.py
@@ -9,7 +9,7 @@ from db.postgresql_db import get_db
 from models.user import User_create, User_base
 from authentication.security import verify_password, get_password_hash
 from authentication.utils import update_user_state
-from account_manager.utils import get_user_db, update_user
+from account_manager.utils import get_user_db, email_is_available, update_user
 
 account_router = APIRouter(tags=["modify account"])
 
@@ -36,7 +36,6 @@ async def update_password(
             field_to_update="password",
             new_var=password_hash,
         )
-        update_user_state(username=user.username, state=False, conn=db)
         return {"Response": "Password successfully updated."}
     else:
         raise HTTPException(
@@ -57,10 +56,16 @@ async def update_email(
             detail="new email can't be the same as old email!",
         )
     user_db = get_user_db(user.username, db)
+    if not email_is_available(username=user_db.username, conn=db, email=new_email):
+        raise HTTPException(
+            status_code=HTTP_400_BAD_REQUEST,
+            detail="There is an account with your new email!",
+        )
     if user_db.email == old_email:
         update_user(
             username=user.username, conn=db, field_to_update="email", new_var=new_email
         )
+        update_user_state(username=user.username, state=False, conn=db)
         return {"Response": "Email successfully updated, please confirm your Email."}
     else:
         raise HTTPException(
--- account_manager/utils.py
+++ account_manager/utils.py
@@ -9,6 +9,19 @@ def get_user_db(username: str, conn: Session):
     return user_db
 
 
+def get_user_db_by_email(email: str, conn: Session):
+    user_db = conn.query(models.User).filter(models.User.email == email).first()
+    return user_db
+
+
+def email_is_available(username: str, conn: Session, email: str):
+    email_user = get_user_db_by_email(email, conn)
+    if email_user and email_user.username != username:
+        return False
+    else:
+        return True
+
+
 def update_user(username: str, conn: Session, field_to_update: str, new_var: str):
     user_db = (
         conn.query(models.User)
